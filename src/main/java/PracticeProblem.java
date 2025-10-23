58fc5cd546edc9f11157010aa87d21276157896f
wesley-cheung-ics4ui-01-25 <166552362+wesley-cheung-ics4ui-01-25@users.noreply.github.com>
2025-10-23 17:44:27 +0000

    update practiceproblems

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -23,5 +23,27 @@ public class PracticeProblem {
 	public static void q5() {
 		//Write question 5 code here
 	}
-
+	public static int sum2D(int[][] nums) {
+		int sum = 0;
+		for (int i = 0;i<nums.length;i++) {
+			for (int j = 0;j<nums[i].length;j++) {
+				sum += nums[i][j];
+			}
+		}
+		return sum;
+	}
+	public static int sumRow(int[][] nums, int row) {
+		int sum = 0;
+		for (int j = 0;j<nums[row].length;j++) {
+				sum += nums[row][j];
+			}
+			return sum;
+		}
+	public static int sumColumn(int[][] nums, int column) {
+		int sum = 0;
+		for (int j = 0;j<nums.length;j++) {
+				sum += nums[j][column];
+			}
+		return sum;
+		}
 }
